4596dd34c76d915c9a103ffb92c0000d9e2f3d24
Anton Macmillan-Parks <anton.macmillan.parks@myraindrop.co.uk>
2023-07-28 13:39:40 +0100

    Remove for_each for temporal cluster

--- modules/aws_ecs/temporal/main.tf
+++ modules/aws_ecs/temporal/main.tf
@@ -40,7 +40,7 @@ module "temporal_aurora_rds" {
   }
 }
 
-resource "aws_service_discovery_service" "temporal_frontend_service" { 
+resource "aws_service_discovery_service" "temporal_frontend_service" {
   name  = "temporal"
 
   dns_config {
@@ -59,14 +59,11 @@ resource "aws_service_discovery_service" "temporal_frontend_service" {
   }
 }
 
-resource "aws_ecs_service" "retool_temporal" {
-
-  for_each = var.temporal_services_config
-
-  name            = "${var.deployment_name}-${each.key}"
+resource "aws_ecs_service" "retool_temporal_frontend" {
+  name            = "${var.deployment_name}-frontend"
   cluster         = var.aws_ecs_cluster_id
   desired_count   = 1
-  task_definition = aws_ecs_task_definition.retool_temporal[each.key].arn
+  task_definition = aws_ecs_task_definition.retool_temporal_frontend.arn
 
   # Need to explictly set this in aws_ecs_service to avoid destructive behavior: https://github.com/hashicorp/terraform-provider-aws/issues/22823
   capacity_provider_strategy {
@@ -76,7 +73,7 @@ resource "aws_ecs_service" "retool_temporal" {
   }
 
   dynamic "service_registries" {
-    for_each = each.key == "frontend" ? toset([1]) : toset([])
+    for_each = toset([1])
     content {
       registry_arn = aws_service_discovery_service.temporal_frontend_service.arn
     }
@@ -85,7 +82,7 @@ resource "aws_ecs_service" "retool_temporal" {
   dynamic "network_configuration" {
     for_each = var.launch_type == "FARGATE" ? toset([1]) : toset([])
 
-    content {    
+    content {
       subnets = var.subnet_ids
       security_groups = [
         var.container_sg_id
@@ -95,25 +92,124 @@ resource "aws_ecs_service" "retool_temporal" {
   }
 }
 
-resource "aws_ecs_task_definition" "retool_temporal" {
+resource "aws_ecs_service" "retool_temporal_history" {
+  name            = "${var.deployment_name}-history"
+  cluster         = var.aws_ecs_cluster_id
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.retool_temporal_history.arn
+
+  # Need to explictly set this in aws_ecs_service to avoid destructive behavior: https://github.com/hashicorp/terraform-provider-aws/issues/22823
+  capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = var.launch_type == "FARGATE" ? "FARGATE" : var.aws_ecs_capacity_provider_name
+  }
+
+  dynamic "service_registries" {
+    for_each = toset([])
+    content {
+      registry_arn = aws_service_discovery_service.temporal_frontend_service.arn
+    }
+  }
+
+  dynamic "network_configuration" {
+    for_each = var.launch_type == "FARGATE" ? toset([1]) : toset([])
+
+    content {
+      subnets = var.subnet_ids
+      security_groups = [
+        var.container_sg_id
+      ]
+      assign_public_ip = true
+    }
+  }
+}
+
+resource "aws_ecs_service" "retool_temporal_matching" {
+  name            = "${var.deployment_name}-matching"
+  cluster         = var.aws_ecs_cluster_id
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.retool_temporal_matching.arn
+
+  # Need to explictly set this in aws_ecs_service to avoid destructive behavior: https://github.com/hashicorp/terraform-provider-aws/issues/22823
+  capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = var.launch_type == "FARGATE" ? "FARGATE" : var.aws_ecs_capacity_provider_name
+  }
+
+  dynamic "service_registries" {
+    for_each = toset([])
+    content {
+      registry_arn = aws_service_discovery_service.temporal_frontend_service.arn
+    }
+  }
+
+  dynamic "network_configuration" {
+    for_each = var.launch_type == "FARGATE" ? toset([1]) : toset([])
+
+    content {
+      subnets = var.subnet_ids
+      security_groups = [
+        var.container_sg_id
+      ]
+      assign_public_ip = true
+    }
+  }
+}
+
+resource "aws_ecs_service" "retool_temporal_worker" {
+  name            = "${var.deployment_name}-worker"
+  cluster         = var.aws_ecs_cluster_id
+  desired_count   = 1
+  task_definition = aws_ecs_task_definition.retool_temporal_worker.arn
+
+  # Need to explictly set this in aws_ecs_service to avoid destructive behavior: https://github.com/hashicorp/terraform-provider-aws/issues/22823
+  capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = var.launch_type == "FARGATE" ? "FARGATE" : var.aws_ecs_capacity_provider_name
+  }
+
+  dynamic "service_registries" {
+    for_each = toset([])
+    content {
+      registry_arn = aws_service_discovery_service.temporal_frontend_service.arn
+    }
+  }
+
+  dynamic "network_configuration" {
+    for_each = var.launch_type == "FARGATE" ? toset([1]) : toset([])
+
+    content {
+      subnets = var.subnet_ids
+      security_groups = [
+        var.container_sg_id
+      ]
+      assign_public_ip = true
+    }
+  }
+}
+
+resource "aws_ecs_task_definition" "retool_temporal_frontend" {
 
   for_each = var.temporal_services_config
 
-  family        = "${var.deployment_name}-${each.key}"
+  family        = "${var.deployment_name}-frontend"
   task_role_arn = aws_iam_role.task_role.arn
   execution_role_arn = var.launch_type == "FARGATE" ? aws_iam_role.execution_role[0].arn : null
   requires_compatibilities = var.launch_type == "FARGATE" ? ["FARGATE"] : null
   network_mode  = var.launch_type == "FARGATE" ? "awsvpc" : "bridge"
-  cpu       = var.launch_type == "FARGATE" ? each.value["cpu"] : null
-  memory    = var.launch_type == "FARGATE" ? each.value["memory"] : null
+  cpu       = var.launch_type == "FARGATE" ? 512 : null
+  memory    = var.launch_type == "FARGATE" ? 1024 : null
   container_definitions = jsonencode(
     [
       {
-        name      = "${var.deployment_name}-${each.key}"
+        name      = "${var.deployment_name}-frontend"
         essential = true
         image     = var.temporal_image
-        cpu       = var.launch_type == "EC2" ? each.value["cpu"] : null
-        memory    = var.launch_type == "EC2" ? each.value["memory"] : null
+        cpu       = var.launch_type == "EC2" ? 512 : null
+        memory    = var.launch_type == "EC2" ? 1024 : null
 
         logConfiguration = {
           logDriver = "awslogs"
@@ -126,13 +222,13 @@ resource "aws_ecs_task_definition" "retool_temporal" {
 
         portMappings = [
           {
-            containerPort = each.value["request_port"]
-            hostPort      = each.value["request_port"]
+            containerPort = 7233
+            hostPort      = 7233
             protocol      = "tcp"
           },
           {
-            containerPort = each.value["membership_port"]
-            hostPort      = each.value["membership_port"]
+            containerPort = 6933
+            hostPort      = 6933
             protocol      = "tcp"
           }
         ]
@@ -142,14 +238,192 @@ resource "aws_ecs_task_definition" "retool_temporal" {
           [
             {
               "name"  = "SERVICES"
-              "value" = each.key
+              "value" = "frontend"
+            },
+          ]
+        )
+      }
+    ]
+  )
+}
+
+resource "aws_ecs_task_definition" "retool_temporal_history" {
+
+  for_each = var.temporal_services_config
+
+  family        = "${var.deployment_name}-history"
+  task_role_arn = aws_iam_role.task_role.arn
+  execution_role_arn = var.launch_type == "FARGATE" ? aws_iam_role.execution_role[0].arn : null
+  requires_compatibilities = var.launch_type == "FARGATE" ? ["FARGATE"] : null
+  network_mode  = var.launch_type == "FARGATE" ? "awsvpc" : "bridge"
+  cpu       = var.launch_type == "FARGATE" ? 512 : null
+  memory    = var.launch_type == "FARGATE" ? 2048 : null
+  container_definitions = jsonencode(
+    [
+      {
+        name      = "${var.deployment_name}-history"
+        essential = true
+        image     = var.temporal_image
+        cpu       = var.launch_type == "EC2" ? 512 : null
+        memory    = var.launch_type == "EC2" ? 2048 : null
+
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = var.aws_cloudwatch_log_group_id
+            awslogs-region        = var.aws_region
+            awslogs-stream-prefix = "SERVICE_RETOOL_TEMPORAL"
+          }
+        }
+
+        portMappings = [
+          {
+            containerPort = 7234
+            hostPort      = 7234
+            protocol      = "tcp"
+          },
+          {
+            containerPort = 6934
+            hostPort      = 6934
+            protocol      = "tcp"
+          }
+        ]
+
+        environment = concat(
+          local.environment_variables,
+          [
+            {
+              "name"  = "SERVICES"
+              "value" = "history"
             },
           ],
-          each.key != "frontend" ? [ {
+          [ {
               "name": "PUBLIC_FRONTEND_ADDRESS",
               "value": "${var.temporal_cluster_config.host}:${var.temporal_cluster_config.port}"
             }
-          ] : []
+          ]
+        )
+      }
+    ]
+  )
+}
+
+resource "aws_ecs_task_definition" "retool_temporal_matching" {
+
+  for_each = var.temporal_services_config
+
+  family        = "${var.deployment_name}-matching"
+  task_role_arn = aws_iam_role.task_role.arn
+  execution_role_arn = var.launch_type == "FARGATE" ? aws_iam_role.execution_role[0].arn : null
+  requires_compatibilities = var.launch_type == "FARGATE" ? ["FARGATE"] : null
+  network_mode  = var.launch_type == "FARGATE" ? "awsvpc" : "bridge"
+  cpu       = var.launch_type == "FARGATE" ? 512 : null
+  memory    = var.launch_type == "FARGATE" ? 1024 : null
+  container_definitions = jsonencode(
+    [
+      {
+        name      = "${var.deployment_name}-matching"
+        essential = true
+        image     = var.temporal_image
+        cpu       = var.launch_type == "EC2" ? 512 : null
+        memory    = var.launch_type == "EC2" ? 1024 : null
+
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = var.aws_cloudwatch_log_group_id
+            awslogs-region        = var.aws_region
+            awslogs-stream-prefix = "SERVICE_RETOOL_TEMPORAL"
+          }
+        }
+
+        portMappings = [
+          {
+            containerPort = 7235
+            hostPort      = 7235
+            protocol      = "tcp"
+          },
+          {
+            containerPort = 6935
+            hostPort      = 6935
+            protocol      = "tcp"
+          }
+        ]
+
+        environment = concat(
+          local.environment_variables,
+          [
+            {
+              "name"  = "SERVICES"
+              "value" = "matching"
+            },
+          ],
+          [ {
+              "name": "PUBLIC_FRONTEND_ADDRESS",
+              "value": "${var.temporal_cluster_config.host}:${var.temporal_cluster_config.port}"
+            }
+          ]
+        )
+      }
+    ]
+  )
+}
+
+resource "aws_ecs_task_definition" "retool_temporal_worker" {
+
+  for_each = var.temporal_services_config
+
+  family        = "${var.deployment_name}-worker"
+  task_role_arn = aws_iam_role.task_role.arn
+  execution_role_arn = var.launch_type == "FARGATE" ? aws_iam_role.execution_role[0].arn : null
+  requires_compatibilities = var.launch_type == "FARGATE" ? ["FARGATE"] : null
+  network_mode  = var.launch_type == "FARGATE" ? "awsvpc" : "bridge"
+  cpu       = var.launch_type == "FARGATE" ? 512 : null
+  memory    = var.launch_type == "FARGATE" ? 1024 : null
+  container_definitions = jsonencode(
+    [
+      {
+        name      = "${var.deployment_name}-worker"
+        essential = true
+        image     = var.temporal_image
+        cpu       = var.launch_type == "EC2" ? 512 : null
+        memory    = var.launch_type == "EC2" ? 1024 : null
+
+        logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group         = var.aws_cloudwatch_log_group_id
+            awslogs-region        = var.aws_region
+            awslogs-stream-prefix = "SERVICE_RETOOL_TEMPORAL"
+          }
+        }
+
+        portMappings = [
+          {
+            containerPort = 7239
+            hostPort      = 7239
+            protocol      = "tcp"
+          },
+          {
+            containerPort = 6939
+            hostPort      = 6939
+            protocol      = "tcp"
+          }
+        ]
+
+        environment = concat(
+          local.environment_variables,
+          [
+            {
+              "name"  = "SERVICES"
+              "value" = "worker"
+            },
+          ],
+          [ {
+              "name": "PUBLIC_FRONTEND_ADDRESS",
+              "value": "${var.temporal_cluster_config.host}:${var.temporal_cluster_config.port}"
+            }
+          ]
         )
       }
     ]
