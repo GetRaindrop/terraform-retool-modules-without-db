379040605829277184e15585502615f6f74baee6
Anton Macmillan-Parks <anton.macmillan.parks@myraindrop.co.uk>
2023-08-02 22:18:19 +0100

    Fix ingress

--- modules/aws_ecs/variables.tf
+++ modules/aws_ecs/variables.tf
@@ -355,7 +355,7 @@ variable "alb_ingress_rules" {
     {
       description      = "Global HTTPS inbound"
       from_port        = "443"
-      to_port          = "80"
+      to_port          = "443"
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
