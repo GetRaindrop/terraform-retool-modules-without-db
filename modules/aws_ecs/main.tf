039399a2ecaf2a8e04912b79c3be0b61eaffdbb7
Anton Macmillan-Parks <anton.macmillan.parks@myraindrop.co.uk>
2024-10-31 18:26:25 +0000

    Update max allocated storage

--- modules/aws_ecs/main.tf
+++ modules/aws_ecs/main.tf
@@ -40,6 +40,7 @@ resource "aws_db_instance" "this" {
   vpc_security_group_ids       = [aws_security_group.rds.id]
   db_subnet_group_name         = aws_db_subnet_group.this.name
   performance_insights_enabled = var.rds_performance_insights_enabled
+  max_allocated_storage        = "645"
 
   skip_final_snapshot = true
   apply_immediately   = true
